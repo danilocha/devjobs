4801d2dca291d7d0b8432838d717450cf6a65bbe
daniel lopez <danilocha1@gmail.com>
2019-10-29 17:48:41 -0500

    arreglando puerto

--- config/email.js
+++ config/email.js
@@ -1,6 +1,6 @@
 module.exports = {
-  user: "f2a56423b5aee5",
-  pass: "82991ce36df852",
-  host: "smtp.mailtrap.io",
-  port: 2525
+  user: "not.reply.devjobs@gmail.com",
+  pass: "devjobs.reply.not",
+  host: "smtp.gmail.com",
+  port: 587
 };
